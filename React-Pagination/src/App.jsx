caa5cc2a7a25c3f570d505a2db50e1e443a01d19
Jemuel <jemuel.rivero1291@gmail.com>
2026-02-23 23:58:33 +0800

    code modification

--- React-Pagination/src/App.jsx
+++ React-Pagination/src/App.jsx
@@ -3,18 +3,17 @@ import './App.css'
 
 function App() {
   const [products, setProducts] = useState(0);
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0)
 
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products?Limit100");
+    const res = await fetch(`https://dummyjson.com/products?Limit=100&&skip=${page * 10 - 10}`);
     const data = await res.json();
 
     if (data && data.products) {
       setProducts(data.products)
     }
   };
-
-  // console.log(products)
 
   useEffect(() => {
     fetchProducts()
@@ -23,6 +22,7 @@ function App() {
   const selectedPageHandler = (selectedPage) => {
     if (selectedPage > 0 && selectedPage <= products.length && selectedPage !== page) {
       setPage(selectedPage)
+      setTotalPages(data.total)
     }
   }
 
@@ -31,7 +31,7 @@ function App() {
       {
           products.length > 0 && <div className='products'>
             {
-              products.slice(page * 10 - 10, page * 10).map((prod) => {
+              products.map((prod) => {
                 return <span className='product__single' key ={prod.id}>
                   <img src={prod.thumbnail} alt={prod.title} />
                   <span>{[prod.title]}</span>
