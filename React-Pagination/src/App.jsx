ef7fb8ab554c12f36b8ae1faf0389ee115319613
Jemuel <jemuel.rivero1291@gmail.com>
2026-02-23 23:45:42 +0800

    add and display pagination buttons

--- React-Pagination/src/App.jsx
+++ React-Pagination/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [products, setProducts] = useState(0);
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(1)
 
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?Limit100");
@@ -14,24 +14,47 @@ function App() {
     }
   };
 
-  console.log(products)
+  // console.log(products)
 
   useEffect(() => {
     fetchProducts()
   }, []);
+
+  const selectedPageHandler = (selectedPage) => {
+    if (selectedPage > 0 && selectedPage <= products.length && selectedPage !== page) {
+      setPage(selectedPage)
+    }
+  }
 
   return (
     <>
       {
           products.length > 0 && <div className='products'>
             {
-              products.map((prod) => {
+              products.slice(page * 10 - 10, page * 10).map((prod) => {
                 return <span className='product__single' key ={prod.id}>
                   <img src={prod.thumbnail} alt={prod.title} />
                   <span>{[prod.title]}</span>
                 </span>;
               })
             }
+          </div>
+        }
+        {
+          products.length > 0 && <div className='pagination'>
+            <span onClick={()=>selectedPageHandler(page - 1)}
+              className={page !== 1 ? "" : "pagination__disable"}>Previous</span>
+            {
+              [...Array(products.length / 10)].map((_,i) => {
+                return <span onClick={()=>selectedPageHandler(i + 1)} 
+                key={i} 
+                className={page === i + 1 ? "pagination__selected" : ""}>
+                  {i+1}
+                </span>
+              })
+            }
+            <span onClick={()=>selectedPageHandler(page + 1)}
+            className={page < products.length / 10 ? "" : "pagination__disable"}>Next</span>
           </div>
         }
     </>
